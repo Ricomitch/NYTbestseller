c4b93a0fd752d5d820cd99d6fa86afd1d1d1e76e
Rico Mitchell <ricomitch@icloud.com>
2020-03-11 04:27:03 +0000

    add notes for cli class

--- lib/NYTbestseller/cli.rb
+++ lib/NYTbestseller/cli.rb
@@ -1,8 +1,26 @@
 class NYTbestseller::CLI
   
+  #start should greet user, maybe give desc 
+  #get data from API 
+  #create new custom objs
+  #all inside start method 
+  
   def start
     puts "Hello there!"
-    binding.pry
-  end  
+    puts "getting data from API............. Please Wait"
+    puts "creating new objs"
+  end 
+  
+  #deal with inputs (loop to keep asking to make a new book selection)
+  #display a list of books
+  #get user input 
+  #condition to check input for good value 
+  #else tell them try again
+  
+  #exit command 
+  #if input == "exit"
+  #say goodbye and kill program
+  
+   
   
 end  
