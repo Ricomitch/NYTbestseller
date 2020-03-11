404709c33c846b9609b6b97cedfba2a47b19673e
Rico Mitchell <ricomitch@icloud.com>
2020-03-11 04:13:49 +0000

    add pry

--- lib/NYTbestseller.rb
+++ lib/NYTbestseller.rb
@@ -1,6 +1,9 @@
 require "NYTbestseller/version"
 require "NYTbestseller/cli"
 
+# dependencies
+require "pry"
+
 module NYTbestseller
   class Error < StandardError; end
   # Your code goes here...
--- lib/NYTbestseller/cli.rb
+++ lib/NYTbestseller/cli.rb
@@ -2,6 +2,7 @@ class NYTbestseller::CLI
   
   def start
     puts "Hello there!"
+    binding.pry
   end  
   
 end  
