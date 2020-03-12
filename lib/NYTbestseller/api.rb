f5d1945b12c04139fec2e371815e9f40c8f51113
Rico Mitchell <ricomitch@icloud.com>
2020-03-12 03:55:31 +0000

    set up objects in api.rb

--- lib/NYTbestseller/api.rb
+++ lib/NYTbestseller/api.rb
@@ -4,10 +4,15 @@ class NYTbestseller::API
     key = "uKB773QWY7gamOVHTTxzXAROjA1YvQAC"
     url = "https://api.nytimes.com/svc/books/v3/lists/{date}/{list}.json?api-key=#{key}"
     
-    
     response = HTTParty.get(url)
     response["results"]["books"].each do |book|
-    
+      rank = book["rank"]
+      title = book["title"]
+      author = book["author"]
+      publisher = book["publisher"]
+      description = book["description"]
+      
+    NYTbestseller::Books.new(rank,title,author,publisher,description)
     binding.pry  
       
   end  
--- lib/NYTbestseller/cli.rb
+++ lib/NYTbestseller/cli.rb
@@ -14,35 +14,4 @@ class NYTbestseller::CLI
   end
   
   
-  def greeting
-    puts "==================Greetings welcome to the New York times Bestsellers list!=================="
-  end
-  
-  
-  def bestseller_list
-    puts "==================Here's the current New York Times Audio Nonfiction Bestsellers:=================="
-  end
-  
-  def book_info
-  end
-  
-  
-   
-      
-      
-  
-  #deal with inputs (loop to keep asking to make a new book selection)
-  #display a list of books
-  #get user input 
-  #condition to check input for good value 
-  #else tell them try again
-  
-  #exit command 
-  #if input == "exit"
-  #say goodbye and kill program
-  
-  def quit 
-    puts "goodbye, until next time!"
-  end  
-  
-end  
+end
